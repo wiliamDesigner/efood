90c0a0636fdcd8a5489653d99b4feb34728d1565
WiliamDev <wiliam.omura3@hotmail.com>
2025-07-01 21:59:57 -0300

    1.6
    Upzinho , indo no caminho certo

--- src/components/Cardlista/index.tsx
+++ src/components/Cardlista/index.tsx
@@ -2,17 +2,15 @@ import { Cardes, CardItem, Sushi } from "../Cardlista/styles";
 import imagensuchi from "../../assets/sushi.png";
 
 const Cardlist = () => (
-  <Cardes>
-    <CardItem>
-      <Sushi src={imagensuchi} alt="Imagens de suchi" />
-      <p>Possivel texto</p>
-    </CardItem>
-    <CardItem>wiliam</CardItem>
-    <CardItem>Vera</CardItem>
-    <CardItem>Jorge</CardItem>
-    <CardItem>gustavo</CardItem>
-    <CardItem>wiliam</CardItem>
-  </Cardes>
+  <div>
+    <Sushi src={imagensuchi} alt="Imagens de suchi" />
+    <Cardes>
+      <CardItem>primeiro card</CardItem>
+      <CardItem>outro card2</CardItem>
+      <CardItem>outro card3</CardItem>
+      <CardItem>outro card4</CardItem>
+    </Cardes>
+  </div>
 );
 
 export default Cardlist;
--- src/components/Cardlista/styles.ts
+++ src/components/Cardlista/styles.ts
@@ -1,30 +1,34 @@
 import styled from "styled-components";
 import { cores } from "../../Styles";
 
-export const CardItem = styled.div`
-  width: 472px;
-  height: 398px;
-  background-color: ${cores.vermelha};
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  gap: 40px;
-  color: white;
-  font-size: 20px;
-`;
-
 export const Cardes = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: auto;
-  gap: 80px;
+  gap: 40px;
   align-items: start;
   background-color: ${cores.branco_mais_claro};
   color: ${cores.vermelha};
+  padding-top: 220px;
 `;
+
+export const CardItem = styled.div`
+  width: 472px;
+  height: 181px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-direction: column;
+  gap: 40px;
+  color: ${cores.vermelha};
+  border: 1px solid ${cores.vermelha};
+  font-size: 18px;
+  font-family: "Roboto";
+`;
+
 export const Sushi = styled.img`
+  grid-template-rows: auto;
+  position: absolute;
   width: 472px;
   height: 217px;
 `;
